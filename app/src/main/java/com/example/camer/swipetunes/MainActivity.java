0a50fa32f879a422b46287f6be712fe7f2e9d33d
lau <laura.dnce@gmail.com>
2018-03-17 18:05:24 -0400

    Laura: Init SongActivity

--- app/src/main/java/com/example/camer/swipetunes/MainActivity.java
+++ app/src/main/java/com/example/camer/swipetunes/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.camer.swipetunes;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity
 {
@@ -10,5 +13,19 @@ public class MainActivity extends AppCompatActivity
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button songsButton = findViewById(R.id.songsButton);
+        songsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                songsButtonOnClick();
+            }
+        });
     }
+
+    private void songsButtonOnClick() {
+        Intent intent = new Intent(this, SongActivity.class);
+        startActivity(intent);
+    }
+
 }
