c865c6fd531d8337c9f7daff87aca257ae07a903
camerondurr <cameronmdurr@yahoo.com>
2018-03-20 22:45:45 -0400

    Added default GestureDetector to MainActivity.

--- app/src/main/java/com/example/camer/swipetunes/MainActivity.java
+++ app/src/main/java/com/example/camer/swipetunes/MainActivity.java
@@ -1,13 +1,23 @@
 package com.example.camer.swipetunes;
 
 import android.content.Intent;
+import android.support.v4.view.GestureDetectorCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.GestureDetector;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity
+public class MainActivity extends AppCompatActivity implements
+        GestureDetector.OnGestureListener,
+        GestureDetector.OnDoubleTapListener
 {
+    // GestureDetector
+    private static final String DEBUG_TAG = "Gestures";
+    private GestureDetectorCompat mDetector;
+
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
@@ -21,6 +31,9 @@ public class MainActivity extends AppCompatActivity
                 songsButtonOnClick();
             }
         });
+
+        mDetector = new GestureDetectorCompat(this,this);
+        mDetector.setOnDoubleTapListener(this);
     }
 
     private void songsButtonOnClick() {
@@ -28,4 +41,66 @@ public class MainActivity extends AppCompatActivity
         startActivity(intent);
     }
 
+    // GestureDetector
+    @Override
+    public boolean onTouchEvent(MotionEvent event)
+    {
+        if (this.mDetector.onTouchEvent(event))
+        {
+            return true;
+        }
+        return super.onTouchEvent(event);
+    }
+    @Override
+    public boolean onDown(MotionEvent event)
+    {
+        Log.d(DEBUG_TAG,"onDown: " + event.toString());
+        return true;
+    }
+    @Override
+    public boolean onFling(MotionEvent event1, MotionEvent event2, float velocityX, float velocityY)
+    {
+        Log.d(DEBUG_TAG, "onFling: " + event1.toString() + event2.toString());
+        return true;
+    }
+    @Override
+    public void onLongPress(MotionEvent event)
+    {
+        Log.d(DEBUG_TAG, "onLongPress: " + event.toString());
+    }
+    @Override
+    public boolean onScroll(MotionEvent event1, MotionEvent event2, float distanceX, float distanceY)
+    {
+        Log.d(DEBUG_TAG, "onScroll: " + event1.toString() + event2.toString());
+        return true;
+    }
+    @Override
+    public void onShowPress(MotionEvent event)
+    {
+        Log.d(DEBUG_TAG, "onShowPress: " + event.toString());
+    }
+    @Override
+    public boolean onSingleTapUp(MotionEvent event)
+    {
+        Log.d(DEBUG_TAG, "onSingleTapUp: " + event.toString());
+        return true;
+    }
+    @Override
+    public boolean onDoubleTap(MotionEvent event)
+    {
+        Log.d(DEBUG_TAG, "onDoubleTap: " + event.toString());
+        return true;
+    }
+    @Override
+    public boolean onDoubleTapEvent(MotionEvent event)
+    {
+        Log.d(DEBUG_TAG, "onDoubleTapEvent: " + event.toString());
+        return true;
+    }
+    @Override
+    public boolean onSingleTapConfirmed(MotionEvent event)
+    {
+        Log.d(DEBUG_TAG, "onSingleTapConfirmed: " + event.toString());
+        return true;
+    }
 }
